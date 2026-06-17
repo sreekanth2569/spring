834ef14cb5d36b016b5cda9835bf00298e575bd5
sreekanth002 <sreekanthavuleti3@gmail.com>
2026-06-17 18:45:29 +0530

    kkk

--- src/main/java/com/example/sri/entity/Employee.java
+++ src/main/java/com/example/sri/entity/Employee.java
@@ -21,7 +21,7 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name should ne nfkje kejk kjeve kjkhjr be blank")
+    @NotBlank(message = "Name s fhgdhfbhould ne nfkje kejk kjeve kjkhjr be blank")
     private String name;
 
     private String email;
